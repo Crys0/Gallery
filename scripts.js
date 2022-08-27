c031e66b11f1571f1272310d22041f5fc437fefe
Crys0 <36264200+Crys0@users.noreply.github.com>
2022-08-27 17:01:29 +0200

    Main

--- scripts.js
+++ scripts.js
@@ -5,9 +5,18 @@ document.addEventListener("DOMContentLoaded", function () {
   let allImages = document.querySelectorAll("img");
   let currentImage = 0;
   let allCircles = document.querySelectorAll(".circle");
-  let currentCircle = 0;
 
-  BUTTON_RIGHT.addEventListener("click", () => {
+  function check() {
+    for (let i = 0; i < allImages.length; i++) {
+      if (!allImages[i].classList.contains("invisible")) {
+        allCircles[i].classList.add("Active");
+      } else {
+        allCircles[i].classList.remove("Active");
+      }
+    }
+  }
+
+  function onClickRight() {
     if (currentImage > allImages.length - 2) {
       currentImage = 0;
       allImages[currentImage].classList.remove("invisible");
@@ -17,10 +26,9 @@ document.addEventListener("DOMContentLoaded", function () {
       allImages[currentImage + 1].classList.remove("invisible");
       currentImage++;
     }
-    
-  });
+  }
 
-  BUTTON_LEFT.addEventListener("click", () => {
+  function onClickLeft() {
     if (currentImage <= 0) {
       allImages[currentImage].classList.add("invisible");
       allImages[allImages.length - 1].classList.remove("invisible");
@@ -30,5 +38,15 @@ document.addEventListener("DOMContentLoaded", function () {
       allImages[currentImage - 1].classList.remove("invisible");
       currentImage--;
     }
+  }
+
+  BUTTON_RIGHT.addEventListener("click", () => {
+    onClickRight();
+    check();
+  });
+
+  BUTTON_LEFT.addEventListener("click", () => {
+    onClickLeft();
+    check();
   });
 });
